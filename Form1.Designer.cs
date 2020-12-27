f51ceac0f08da326d02c81d7029c5f98d39ad402
fieldman <fieldman@inbox.ru>
2020-12-27 21:01:09 +0700

    Добавлена работа комобоксов

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,9 +32,11 @@ namespace MenuStrip
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -62,16 +64,27 @@ namespace MenuStrip
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.comboBox2);
             this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Location = new System.Drawing.Point(8, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(311, 105);
+            this.groupBox1.Size = new System.Drawing.Size(311, 124);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGray;
+            this.button2.Location = new System.Drawing.Point(200, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Отправить";
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // label2
             // 
@@ -94,16 +107,40 @@ namespace MenuStrip
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(73, 51);
+            this.comboBox2.Items.AddRange(new object[] {
+            "50",
+            "75",
+            "110",
+            "150",
+            "300",
+            "600",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(73, 76);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 23);
             this.comboBox2.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(-1, 340);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(504, 111);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(502, 450);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -122,6 +159,8 @@ namespace MenuStrip
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -23,33 +23,50 @@ namespace MenuStrip
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            BtnStat = !BtnStat;
-            if (BtnStat == false)
             {
-                button1.Text = "Открыть";
-                button1.BackColor = Color.LightGray;
-            }
-            else
-            {
-                button1.Text = "Закрыть";
-                button1.BackColor = Color.Lime;
+                if (comboBox1.Items.Count > 0)
+                {
+                    BtnStat = true;
+
+                    System.IO.Ports.SerialPort serialPort1;
+                    //System.ComponentModel.IContainer components = new System.ComponentModel.Container(); // <-- ЭТО ЧТО ЗА СТРАШНЫЙ ЗВЕРЬ??? и на хрено оно нужно?;
+                    serialPort1 = new System.IO.Ports.SerialPort(components);
+                    serialPort1.PortName = comboBox1.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(comboBox2.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Выберите COM порт.");
+                    BtnStat = false;
+                }
+                // цветовая обработка события и название кнопки
+                if (BtnStat == false)
+                {
+                    button1.Text = "Открыть";
+                    button1.BackColor = Color.LightGray;
+                }
+                else
+                {
+                    button1.Text = "Закрыть";
+                    button1.BackColor = Color.Lime;
+                }
+
+
             }
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            //string[] serialPorts;
+            private void Form1_Load(object sender, EventArgs e)
+            {
+            //
             try
             {
-                // Get a list of serial port names.
-              this.serialPorts = SerialPort.GetPortNames();
+                this.serialPorts = SerialPort.GetPortNames();
                 comboBox1.Items.AddRange(items: serialPorts);
             }
-            catch (Win32Exception)
-            {
-                
+                catch (Win32Exception) { }
+
+            comboBox2.SelectedIndex = 9;   // устанавливаю на комбобоксе2 скорость по умолчанию 9600 (9ю строку)
+            if (comboBox1.Items.Count!=0) { comboBox1.SelectedIndex = 0; } //устанавливаю COM порт, первый по списку.
             }
 
-        }
     }
 }
